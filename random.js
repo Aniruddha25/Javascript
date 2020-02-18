c3d5f9412f10a1b716918f65e0f19e1c37ef2f4c
ANIRUDDHA Kulkarni <41265081+Aniruddha25@users.noreply.github.com>
2020-02-18 22:12:44 +0530

    Add files via upload

---
+++ random.js
@@ -0,0 +1,16 @@
+let pi =22/7
+console.log(pi.toFixed(2))//round off the float value to specified number.
+let a = Math.random()*10//Selects random number between 0 and 1.
+console.log(a)
+let upper = 100
+let lower = 1
+let random_num = Math.floor(Math.random()*(upper-lower+1))+lower
+console.log(random_num)
+
+             
+    
+
+
+      
+
+
